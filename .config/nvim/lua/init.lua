55c2cb31ca0cca9de3e2d74962780ca422990bb6
Davit Mayilyan <6394434+dmayilyan@users.noreply.github.com>
2024-06-07 17:31:52 +0400

    Add fuzzy text search

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -8,16 +8,19 @@ require("mason-lspconfig").setup{
 require("lspconfig").ruff.setup{}
 require("lspconfig").intelephense.setup({})
 
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, {})
+vim.keymap.set('n', '<leader>ss', require('telescope.builtin').grep_string, {})
+
 local on_attach = function(_, _)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
     vim.keymap.set('n', '<leader>rf', vim.lsp.buf.format, {})
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
     vim.keymap.set('n', '<leader>rr', require('telescope.builtin').registers, {})
 
-
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
     vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+    vim.keymap.set('n', 'gs', require('telescope.builtin').git_status, {})
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
@@ -30,7 +33,10 @@ require("lspconfig").pylsp.setup{
 }
 
 local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
 })
